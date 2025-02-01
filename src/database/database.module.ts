0a76d19d2e4f2354271a65ac1c63fd73cea08b2e
qormsxo <geuntae0212@gmail.com>
2025-02-01 13:01:26 +0900

    ✨ typeorm  db 연동

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,9 +1,12 @@
 
 import { Module } from '@nestjs/common';
 import { databaseProviders } from './database.providers';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+    imports: [ConfigModule.forRoot()],
   providers: [...databaseProviders],
   exports: [...databaseProviders],
 })
+
 export class DatabaseModule {}
--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,5 +1,5 @@
 import { DataSource } from 'typeorm';
-
+import * as path from 'path';
 export const databaseProviders = [
   {
     provide: 'DATA_SOURCE',
@@ -8,16 +8,17 @@ export const databaseProviders = [
         type: 'mysql',
         host: 'localhost',
         port: 3306,
-        username: 'root',
-        password: 'root',
-        database: 'test',
+        username: process.env.DBUSER,
+        password: process.env.DBPW,
+        database: 'realworld',
         entities: [
-            __dirname + '/../**/*.entity{.ts,.js}',
+          path.resolve(__dirname, '..', 'modules', '**', '*.entity{.ts,.js}'),
         ],
-        synchronize: true,
+        synchronize: false,
       });
-
+    
       return dataSource.initialize();
     },
   },
 ];
+
